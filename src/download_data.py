ceac0e0caa8f93a98cf1db2391f49ff743b006f5
skj092 <sonujha092@gmail.com>
2025-07-04 15:02:49 +0530

    init

---
+++ src/download_data.py
@@ -0,0 +1,9 @@
+
+# IMPORTANT: RUN THIS CELL IN ORDER TO IMPORT YOUR KAGGLE DATA SOURCES,
+# THEN FEEL FREE TO DELETE THIS CELL.
+# NOTE: THIS NOTEBOOK ENVIRONMENT DIFFERS FROM KAGGLE'S PYTHON
+# ENVIRONMENT SO THERE MAY BE MISSING LIBRARIES USED BY YOUR
+# NOTEBOOK.
+import kagglehub
+chrisfilo_urbansound8k_path = kagglehub.dataset_download('chrisfilo/urbansound8k')
+print('Data source import complete.', chrisfilo_urbansound8k_path)
